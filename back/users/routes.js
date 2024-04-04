c2a3e79056be5fd9cdc9ab79889d09cbc7bb0b49
thomas-spinec <thomas.spinec@laplateforme.io>
2024-04-04 12:01:15 +0200

    Add dev script to package.json and update user routes

--- back/users/routes.js
+++ back/users/routes.js
@@ -1,16 +1,70 @@
+// const router = require("express").Router();
+
+// // Import the user controller
+// const UserController = require("./controllers/UserController");
+
+// // Import the roles schema
+// const { roles } = require("../config");
+
+// // ========================> GET USER (will be the logged in user)
+// router.get("/", UserController.getUser);
+
+// // ========================> GET ALL USERS
+// router.get("/all", UserController.getAllUsers);
+
+// // export the router
+// module.exports = router;
+
+// ==============================================================================
+// =====================================> SUITE <=================================
+
 const router = require("express").Router();
 
-// Import the user controller
+// Middleware Imports
+const isAuthenticatedMiddleware = require("./../common/middlewares/IsAuthenticatedMiddleware");
+// const SchemaValidationMiddleware = require("../common/middlewares/SchemaValidationMiddleware");
+const CheckPermissionMiddleware = require("../common/middlewares/CheckPermissionMiddleware");
+
+// Controller Imports
 const UserController = require("./controllers/UserController");
 
-// Import the roles schema
+// JSON Schema Imports for payload verification
+// const updateUserPayload = require("./schemas/updateUserPayload");
+// const changeRolePayload = require("./schemas/changeRolePayload");
+
 const { roles } = require("../config");
 
-// ========================> GET USER (will be the logged in user)
-router.get("/", UserController.getUser);
+router.get("/", [isAuthenticatedMiddleware.check], UserController.getUser);
 
-// ========================> GET ALL USERS
-router.get("/all", UserController.getAllUsers);
+// router.patch(
+//   "/",
+//   [
+//     isAuthenticatedMiddleware.check,
+//     SchemaValidationMiddleware.verify(updateUserPayload),
+//   ],
+//   UserController.updateUser
+// );
 
-// export the router
+router.get(
+  "/all",
+  [isAuthenticatedMiddleware.check, CheckPermissionMiddleware.has(roles.ADMIN)],
+  UserController.getAllUsers
+);
+
+// router.patch(
+//   "/change-role/:userId",
+//   [
+//     isAuthenticatedMiddleware.check,
+//     CheckPermissionMiddleware.has(roles.ADMIN),
+//     SchemaValidationMiddleware.verify(changeRolePayload),
+//   ],
+//   UserController.changeRole
+// );
+
+router.delete(
+  "/:userId",
+  [isAuthenticatedMiddleware.check, CheckPermissionMiddleware.has(roles.ADMIN)],
+  UserController.deleteUser
+);
+
 module.exports = router;
